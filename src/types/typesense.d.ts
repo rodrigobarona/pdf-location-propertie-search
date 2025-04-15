f1ffd69f61f70aca2c1136cbbb9843fb5e143c06
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-04-15 02:27:22 -0700

    add typensense types

---
+++ src/types/typesense.d.ts
@@ -0,0 +1,32 @@
+declare module "typesense" {
+  // Using namespace instead of static class to avoid linter issues
+  namespace Typesense {
+    class Client {
+      constructor(options: {
+        apiKey: string;
+        nodes: Array<{
+          host: string;
+          port: number;
+          protocol: string;
+        }>;
+        connectionTimeoutSeconds?: number;
+      });
+
+      collections(): {
+        retrieve(): Promise<Record<string, unknown>>;
+        documents(): {
+          search(
+            parameters: Record<string, unknown>
+          ): Promise<Record<string, unknown>>;
+        };
+      };
+    }
+  }
+
+  // Default export
+  const Typesense: {
+    Client: typeof Typesense.Client;
+  };
+
+  export default Typesense;
+}
